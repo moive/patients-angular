61c792221f6913a5112f89eaed58354d202d7846
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-02-12 13:09:53 -0500

    add navbar-dashboar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarDashboardComponent } from './components/navbar-dashboard/navbar-dashboard.component';
 
 @NgModule({
   declarations: [
     viewsComponent,
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarDashboardComponent
   ],
   imports: [
     BrowserModule,
